d1eeed8c0cdefdc21d6c85608b970d87db5dfc52
wil <wil86toga@gmail.com>
2025-08-26 20:04:41 -0700

    add exercise Fibonacci

--- 02_data_structures/generator_ex.py
+++ 02_data_structures/generator_ex.py
@@ -3,18 +3,37 @@ def squares_generator(n):
     for i in range(n):
         yield i**2
 
+print("Result Squares List:")
+print(list(squares_generator(5)))
 gen = squares_generator(5)
 print("Result Squares Generator:")
 for value in gen:
     print(value)
 
-
 # ex2
 def even_generator(n):
-    for i in range(n):
+    for i in range(n + 1):
         if i % 2 == 0:
             yield i
+print("Result Even List:")
+print(list(even_generator(10)))
 print("Result Even Generator:")
 gen = even_generator(10)
 for value in gen:
     print(value)
+
+# ex3
+def fibonacci_generator(n):
+    a = 0 # value 1 by Fibonacci number
+    b = 1 # value 2 by Fibonacci number
+    count = 0 # counter
+
+    while count < n:
+        yield a # Produce sequence currect number
+        a = b
+        b = a + b # Calculate the next Fibonacci number
+        count += 1
+
+fib_sequence = list(fibonacci_generator(10))
+
+print("List numbers Fibonacci are: ", fib_sequence)
